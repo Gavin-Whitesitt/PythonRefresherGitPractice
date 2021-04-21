d9cf7d2796b88c02ec74c8acf15b522f35e33e00
Gavin Whitesitt <gwhitesitt@gmail.com>
2021-04-21 10:34:45 -0700

    Removed finished examples

--- python_sandbox_starter/classes.py
+++ python_sandbox_starter/classes.py
@@ -1,2 +1,17 @@
 # A class is like a blueprint for creating objects. An object has properties and methods(functions) associated with it. Almost everything in Python is an object
 
+import json
+
+items = json.loads('[{"id":1}]')
+
+def greet(greeting, name):
+    """Returns a greeting
+
+    Args:
+        greeting (string): A greet word
+        name (string): A persons name
+
+    Returns:
+        string: A greeting with a name
+    """
+    return f'{greeting} {name}'
