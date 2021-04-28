8d943539b7ca2864c7121aaa828d1fd7b84be377
Gavin Whitesitt <gwhitesitt@gmail.com>
2021-04-28 09:30:15 -0700

    added pipenv: pipfile and pipfile.lock

--- python_sandbox_starter/modules.py
+++ python_sandbox_starter/modules.py
@@ -1,1 +1,27 @@
 # A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
+
+import datetime
+
+today = datetime.date.today()
+
+
+#just import a single function from module
+from datetime import date
+
+today = date.today()
+
+print(today)
+
+import time
+timestamp = time.time()
+
+print(timestamp)
+
+from time import time
+timestamp = time()
+
+print(timestamp)
+
+#Pip Modules
+import camelcase
+
